acd8339607ebb9fb1779c94bbd37a268236e1f27
bgopi23 <baskargopinath01@gmail.com>
2024-03-28 20:57:34 +0800

    Fix assertion

--- src/main/java/seedu/address/logic/commands/NoteCommand.java
+++ src/main/java/seedu/address/logic/commands/NoteCommand.java
@@ -50,7 +50,7 @@ public class NoteCommand extends Command {
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        assert(model) != null;
+        assert(model != null);
         List<Person> lastShownList = model.getFilteredPersonList();
 
         if (index.getZeroBased() >= lastShownList.size()) {
