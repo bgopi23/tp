a9f69f6c103df1b911189e3e3aec3b19d05ddd20
Chee Jing Jie <jingjie.chee@u.nus.edu>
2024-03-30 15:28:52 +0800

    Fix checkstyle

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -12,7 +12,8 @@ public class Phone extends Attribute<String> {
     public static final String MESSAGE_CONSTRAINTS =
         "Phone numbers should only contain digits and have a minimum length of 1.";
     public static final String MESSAGE_EXPECTED =
-        "The phone number supplied does not appear to be from Singapore,\nfor other types of numbers, please ensure that you have keyed it in correctly.";
+        "The phone number supplied does not appear to be from Singapore,\nfor other types of numbers, "
+            + "please ensure that you have keyed it in correctly.";
     public static final String EXPECTED_FORMAT_REGEX = "^[689]\\d{7}$";
     public static final String REQUIRED_REGEX = "^\\d+$";
 
