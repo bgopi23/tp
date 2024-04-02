65ce25ec50d3df575fde50b5be1431fc16bb268b
Chee Jing Jie <jingjie.chee@u.nus.edu>
2024-04-02 23:10:26 +0800

    Move tabpane out of gridpane

--- src/main/java/seedu/address/ui/PersonDetailsPanel.java
+++ src/main/java/seedu/address/ui/PersonDetailsPanel.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import java.util.logging.Logger;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
@@ -69,7 +70,7 @@ public class PersonDetailsPanel extends UiPart<Region> {
     private NumberAxis weightYAxis;
     // private LineChart<String, Number> heightChart;
     // private NumberAxis heightYAxis;
-    private VBox notesBox;
+    private VBox exercisesBox;
 
     /**
      * Creates a new PersonDetailsPanel and clears all fields.
@@ -78,6 +79,14 @@ public class PersonDetailsPanel extends UiPart<Region> {
     public PersonDetailsPanel() {
         super(FXML);
         clear();
+    }
+
+    private Tab getWeightTab() {
+        return trackableFieldsTabPane.getTabs().get(0);
+    }
+
+    private Tab getExerciseTab() {
+        return trackableFieldsTabPane.getTabs().get(1);
     }
 
     /**
@@ -109,50 +118,22 @@ public class PersonDetailsPanel extends UiPart<Region> {
         weightChart.lookup(".chart-vertical-grid-lines").setStyle("-fx-stroke: white;");
         weightChart.requestLayout();
 
-        // Initialize height chart
-        // CategoryAxis hxAxis = new CategoryAxis();
-        // heightYAxis = new NumberAxis();
-        //
-        // hxAxis.setAnimated(false);
-        // hxAxis.setLabel("Date");
-        // hxAxis.lookup(".axis-label").setStyle("-fx-text-fill: white;");
-        //
-        // heightYAxis.setAnimated(false);
-        // heightYAxis.setAutoRanging(false);
-        // heightYAxis.setLabel("Height (cm)");
-        // heightYAxis.lookup(".axis-label").setStyle("-fx-text-fill: white;");
-        //
-        // heightChart = new LineChart<>(hxAxis, heightYAxis);
-        // heightChart.setAnimated(false);
-        // heightChart.setHorizontalGridLinesVisible(false);
-        // heightChart.setVerticalGridLinesVisible(false);
-        // heightChart.setTitle("Height Tracking");
-        // heightChart.setLegendVisible(false);
-        // heightChart.lookup(".chart-title").setStyle("-fx-text-fill: white;");
-        // heightChart.setPrefHeight(200);
-        // heightChart.lookup(".chart-horizontal-grid-lines").setStyle("-fx-stroke: white;");
-        // heightChart.lookup(".chart-vertical-grid-lines").setStyle("-fx-stroke: white;");
-        // heightChart.requestLayout();
-
         // Initialize notes box
-        notesBox = new VBox();
-        notesBox.setSpacing(5);
-        notesBox.setPrefHeight(200);
+        exercisesBox = new VBox();
+        exercisesBox.setPadding(new Insets(10, 10, 10, 10));
+        exercisesBox.setPrefHeight(200);
 
         // Create a scroll pane and set the notes box as its content
-        ScrollPane notesScrollPane = new ScrollPane(notesBox);
-        notesScrollPane.setFitToWidth(true);
-        notesScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        ScrollPane exerciseScrollPane = new ScrollPane(exercisesBox);
+        exerciseScrollPane.setFitToWidth(true);
+        exerciseScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
 
         // Add charts and notes scroll pane to respective tabs
-        Tab weightTab = trackableFieldsTabPane.getTabs().get(0);
+        Tab weightTab = getWeightTab();
         weightTab.setContent(weightChart);
 
-        // Tab heightTab = trackableFieldsTabPane.getTabs().get(1);
-        // heightTab.setContent(heightChart);
-
-        Tab notesTab = trackableFieldsTabPane.getTabs().get(1);
-        notesTab.setContent(notesScrollPane);
+        Tab exerciseTab = getExerciseTab();
+        exerciseTab.setContent(exerciseScrollPane);
     }
 
     /**
@@ -189,71 +170,26 @@ public class PersonDetailsPanel extends UiPart<Region> {
         note.setText(person.getNote().toString());
         qrcode.setImage(new Image(person.getQrCodePath().toUri().toString()));
 
-        // Set trackable fields
-
-        // Set weight
-        // XYChart.Series<String, Number> weightSeries = new XYChart.Series<>();
-        //
-        // weightYAxis.setLowerBound(50);
-        // weightYAxis.setUpperBound(110);
-        //
-        // XYChart.Data<String, Number> weightData = new XYChart.Data<>("Jan 2024", 60);
-        // weightData.setNode(new HoveredThresholdNode("", "60", " kg"));
-        // weightSeries.getData().add(weightData);
-        //
-        // weightData = new XYChart.Data<>("Feb 2024", 100);
-        // weightData.setNode(new HoveredThresholdNode("", "100", " kg"));
-        // weightSeries.getData().add(weightData);
-        //
-        // weightData = new XYChart.Data<>("Mar 2024", 80);
-        // weightData.setNode(new HoveredThresholdNode("", "80", " kg"));
-        // weightSeries.getData().add(weightData);
-
         XYChart.Series<String, Number> weightSeries = generateWeightSeries(person);
         weightChart.getData().clear();
         weightChart.getData().add(weightSeries);
 
-        // Set height
-        // XYChart.Series<String, Number> heightSeries = new XYChart.Series<>();
-        //
-        // heightYAxis.setLowerBound(160);
-        // heightYAxis.setUpperBound(190);
-        //
-        // XYChart.Data<String, Number> heightData = new XYChart.Data<>("Mar 2024", 170);
-        // heightData.setNode(new HoveredThresholdNode("", "170", " cm"));
-        // heightSeries.getData().add(heightData);
-        //
-        // heightData = new XYChart.Data<>("Apr 2024", 175);
-        // heightData.setNode(new HoveredThresholdNode("", "175", " cm"));
-        // heightSeries.getData().add(heightData);
-        //
-        // heightData = new XYChart.Data<>("May 2024", 178);
-        // heightData.setNode(new HoveredThresholdNode("", "178", " cm"));
-        // heightSeries.getData().add(heightData);
-        //
-        // heightChart.getData().clear();
-        // heightChart.getData().add(heightSeries);
-
-        // Set notes
-        // DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd MMM yy HH:mm:ss");
-        // Label noteLabel = new Label((LocalDateTime.now()).format(dateTimeFormatter) + " - " + "Random text");
-
-        Label noteLabel1 = new Label("Burpees   -  Sets: 3, Reps: 10, Rest between sets: 2 min");
-        Label noteLabel2 = new Label("Lunges    -   Sets: 5, Reps: 10, Rest between sets: 1 min");
-        Label noteLabel3 = new Label("Squats    -   Sets: 5, Reps: 10, Rest between sets: 3 min");
-
-        noteLabel1.setStyle("-fx-text-fill: white; -fx-font-size: 14");
-        noteLabel2.setStyle("-fx-text-fill: white; -fx-font-size: 14");
-        noteLabel3.setStyle("-fx-text-fill: white; -fx-font-size: 14");
-
-        notesBox.getChildren().clear();
-        notesBox.getChildren().addAll(noteLabel1, noteLabel2, noteLabel3);
+//        Label noteLabel1 = new Label("Burpees   -  Sets: 3, Reps: 10, Rest between sets: 2 min");
+//        Label noteLabel2 = new Label("Lunges    -   Sets: 5, Reps: 10, Rest between sets: 1 min");
+//        Label noteLabel3 = new Label("Squats    -   Sets: 5, Reps: 10, Rest between sets: 3 min");
+//
+//        noteLabel1.setStyle("-fx-text-fill: white; -fx-font-size: 14");
+//        noteLabel2.setStyle("-fx-text-fill: white; -fx-font-size: 14");
+//        noteLabel3.setStyle("-fx-text-fill: white; -fx-font-size: 14");
+//
+//        exercisesBox.getChildren().clear();
+//        exercisesBox.getChildren().addAll(noteLabel1, noteLabel2, noteLabel3);
 
         // Bind manageability (presence) of node based on presence of value for optional fields
         address.setVisible(!person.getAddress().getValue().isEmpty());
         email.setVisible(!person.getEmail().getValue().isEmpty());
         note.setVisible(!person.getNote().getValue().isEmpty());
-        weightDate.setVisible(!latestWeight.isEmpty());
+        weightDate.setVisible(latestWeight.isPresent());
 
         address.managedProperty().bind(address.visibleProperty());
         email.managedProperty().bind(email.visibleProperty());
@@ -306,9 +242,6 @@ public class PersonDetailsPanel extends UiPart<Region> {
             }
         }
 
-        // XYChart.Data<String, Number> heightData = new XYChart.Data<>("Mar 2024", 170);
-        // heightData.setNode(new HoveredThresholdNode("", "170", " cm"));
-        // heightSeries.getData().add(heightData);
         weightYAxis.setLowerBound(minWeight - 10);
         weightYAxis.setUpperBound(maxWeight + 10);
 
