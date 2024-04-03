4c972b2eaf8827e7de95042ee8510dd0435a960d
RyanNgWH <ryan.ng.dev@tinynamoo.com>
2024-04-03 20:11:37 +0800

    Fix weight search

---
+++ src/main/java/seedu/address/model/person/predicates/AlwaysTruePredicate.java
@@ -0,0 +1,18 @@
+package seedu.address.model.person.predicates;
+
+import seedu.address.model.person.Person;
+
+public class AlwaysTruePredicate extends SearchPredicate<String> {
+    /**
+     * Constructs a predicate to that is always true
+     *
+     */
+    public AlwaysTruePredicate() {
+        super("", null);
+    }
+
+    @Override
+    public boolean test(Person person) {
+        return true;
+    }
+}
