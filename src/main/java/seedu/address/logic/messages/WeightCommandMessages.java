e37b92383cae1a982459af2b1c36cdea6e20afb2
bgopi23 <baskargopinath01@gmail.com>
2024-04-10 10:17:29 +0800

    Update weight message

--- src/main/java/seedu/address/logic/messages/WeightCommandMessages.java
+++ src/main/java/seedu/address/logic/messages/WeightCommandMessages.java
@@ -7,7 +7,8 @@ public class WeightCommandMessages extends Messages {
 
     public static final String COMMAND_WORD = "weight";
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Adds a weight (in kg) to a client. If no weight or '0' is specified, the latest weight value will be removed from the client.\n"
+            + ": Adds a weight (in kg) to a client specified by the index number used in the last person listing.\n"
+            + "If no weight or '0' is specified, the latest weight value will be removed from the client.\n"
             + "Parameters: INDEX [WEIGHT] (must be a positive float)\n"
             + "Example: " + COMMAND_WORD + " 1 "
             + "70.0";
