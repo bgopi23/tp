0a84ee42e5b590017670b50fae83c6cf551c65e4
bgopi23 <baskargopinath01@gmail.com>
2024-04-10 13:05:12 +0800

    Update messages

--- src/main/java/seedu/address/logic/messages/WeightCommandMessages.java
+++ src/main/java/seedu/address/logic/messages/WeightCommandMessages.java
@@ -14,14 +14,14 @@ public class WeightCommandMessages extends Messages {
             + "Parameters: INDEX [WEIGHT] (must be a number between 0 and 5000)\n"
             + "Example: " + COMMAND_WORD + " 1 "
             + "70.0";
-    public static final String WEIGHT_VALUE_HEADER = "Weight: ";
-    public static final String WEIGHT_DATE_HEADER = "Date Recorded: ";
     public static final String MESSAGE_ADD_WEIGHT_SUCCESS =
             "Successfully added weight to client!"
             + MESSAGE_RESULT_DIVIDER;
     public static final String MESSAGE_DELETE_WEIGHT_SUCCESS =
             "Successfully removed weight from client!"
             + MESSAGE_RESULT_DIVIDER;
+    public static final String WEIGHT_VALUE_HEADER = "Weight: ";
+    public static final String WEIGHT_DATE_HEADER = "Date Recorded: ";
     public static final String MESSAGE_INVALID_PARAMETER_WEIGHT = String.format(MESSAGE_INVALID_INDEX, MESSAGE_USAGE);
     public static final String MESSAGE_NO_PARAMETER_WEIGHT = String.format(MESSAGE_NO_PARAMETERS, MESSAGE_USAGE);
     public static final String MESSAGE_INVALID_INDEX_WEIGHT = String.format(MESSAGE_INVALID_INDEX, MESSAGE_USAGE);
