c6847043fec8124bf071e1b8136b239615e2bba4
bgopi23 <baskargopinath01@gmail.com>
2024-04-10 13:26:38 +0800

    Improve formatting

--- src/main/java/seedu/address/logic/messages/FindCommandMessages.java
+++ src/main/java/seedu/address/logic/messages/FindCommandMessages.java
@@ -32,12 +32,12 @@ public class FindCommandMessages extends Messages {
     public static final String MESSAGE_NO_CLIENTS_FOUND = "No clients found!";
     public static final String MESSAGE_ONE_CLIENT_FOUND = "1 client found!";
     public static final String MESSAGE_PERSONS_FOUND_OVERVIEW = "%1$d clients found!";
+    public static final String VALIDATION_REGEX_RANGE = "^\\d+(\\.\\d+)?,\\s*\\d+(\\.\\d+)?$";
     public static final String MESSAGE_USAGE_RANGE = COMMAND_WORD
             + ": Finds using a specified range that is comma-delimited."
             + " FROM parameter must be less than or equal to TO parameter.\n"
             + "Parameters: FROM, TO (both of them must be a positive number greater than or equals to 0).\n"
             + "Example: " + COMMAND_WORD + " w/70, 80";
-    public static final String VALIDATION_REGEX_RANGE = "^\\d+(\\.\\d+)?,\\s*\\d+(\\.\\d+)?$";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT_FIND = String.format(MESSAGE_INVALID_COMMAND_FORMAT,
             MESSAGE_USAGE);
 }
